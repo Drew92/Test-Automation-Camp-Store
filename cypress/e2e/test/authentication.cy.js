61e11bf0aad6e8b30d52d046d32ad31110b957b3
Andrew Gayle <agayle@gmail.com>
2022-12-08 17:53:13 -0500

    modified 'sign in with existing account' test, created home page object

--- cypress/e2e/test/authentication.cy.js
+++ cypress/e2e/test/authentication.cy.js
@@ -1,11 +1,14 @@
 /// <reference types="cypress" />
 
 import AuthPage from '../page_objects/auth_pages'
+import HomePage from '../page_objects/home_and_product_gallery_page'
 
 describe("Authentication Tests",()=>{
     
-    it("Login",()=>{
+    it("Sign in with existing account",()=>{
         AuthPage.login("drewtest@ahmail.com","bc123Quality");
+        cy.get(HomePage.btnSignOut).should('exist');
+
     });
     
 
