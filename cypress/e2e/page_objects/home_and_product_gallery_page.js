6425ade262d1e3f5eb880e279c8ae88b8bdc5f70
Andrew Gayle <agayle@gmail.com>
2022-12-13 08:57:05 -0500

    almost complete home page tests, iframe test is incomplete and commented out. Replaced with About page test

--- cypress/e2e/page_objects/home_and_product_gallery_page.js
+++ cypress/e2e/page_objects/home_and_product_gallery_page.js
@@ -6,5 +6,30 @@ class HomePage extends Page {
         return (`#top-sign-out`);
     }    
 
+    get imgQFittedHat(){
+        return (`.chakra-image.css-2i84d9[src='/images/quality-hat-model.jpg']`);
+    } 
+
+    get btnHome(){
+        return (`#top-home`);
+    }  
+
+    get iFrame(){
+        return (`iframe[title='Automate Your Quality Assurance Testing']`);
+    }
+    
+    get iFrameVideo(){
+        return (`.video-stream`);
+    } 
+
+    get btnAbout(){
+        return (` #top-about`);
+    } 
+
+    get linkAbout(){
+        return (`a[href='https://qualityworkscg.com/automation-bootcamp/']`);
+    }
+   
+
 }
 module.exports = new HomePage();
---
+++ cypress/e2e/test/home-and-product-gallery-page-tests.cy.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress" />
+
+import AuthPage from '../page_objects/auth_pages'
+import HomePage from '../page_objects/home_and_product_gallery_page'
+
+describe("Home and Product Gallery page Tests",()=>{
+    
+    beforeEach(() => {
+        AuthPage.open();
+        cy.get(AuthPage.btnSignInOrRegister).click();
+        AuthPage.login("drewtest@ahmail.com","bc123Quality");
+        cy.wait(3000);
+      });
+
+    it("Should be able to logout",()=>{
+      
+        //assertions
+
+        //check that user is on home page and logout option is available
+        cy.url().should('eq', Cypress.config().baseUrl + 'products');
+        cy.get(HomePage.btnSignOut).should('be.visible').click();
+        cy.wait(2000);
+
+        //check that user is successfully logged out
+        cy.url().should('eq', Cypress.config().baseUrl);
+        cy.get(AuthPage.btnSignInOrRegister).should('be.visible');
+
+    });
+
+    it("Should be redirected to home and product gallery page when “Home” button is clicked",()=>{
+       
+        //assertions
+
+        //navigate to Fitted Hat product page
+        cy.get(HomePage.imgQFittedHat).scrollIntoView().should('be.visible').click();
+        cy.url().should('not.equal', Cypress.config().baseUrl);
+        cy.wait(3000);
+
+        //navigate to Home and Product gallery page
+        cy.get(HomePage.btnHome).should('be.visible').click();
+        cy.url().should('eq', Cypress.config().baseUrl+ 'products');
+
+    });
+
+    it("Should open About page when about button is clicked",()=>{
+
+        // cy.wait(4000)
+        // cy.get(HomePage.iFrame)
+        // .its('0.ownerDocument.body')
+        // .should('exist')
+        // .then(cy.wrap)
+        // .find(HomePage.iFrameVideo)
+        // .should('be.visible').click();
+
+        //assertions
+        //assertions
+
+        //cy.get(HomePage.imgQFittedHat).scrollIntoView().should('be.en').click();
+
+        cy.get(HomePage.linkAbout)
+        .invoke('removeAttr', 'target') //removes the target attribute from the link which forces the About page to open in a new tab 
+        .click();
+        cy.url().should('include', 'https://qualityworkscg.com/automation-bootcamp/');
+
+
+    });
+    
+
+});
