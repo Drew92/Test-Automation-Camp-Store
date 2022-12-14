86e07872779eb322711e91a569b0e3078bc29b2c
Andrew Gayle <agayle@gmail.com>
2022-12-14 13:09:54 -0500

    completed 2 tests for product details page

--- cypress/e2e/page_objects/home_and_product_gallery_page.js
+++ cypress/e2e/page_objects/home_and_product_gallery_page.js
@@ -4,7 +4,11 @@ class HomePage extends Page {
 
     get btnSignOut(){
         return (`#top-sign-out`);
-    }    
+    }  
+    
+    get imgQFittedHat(){
+        return (`.chakra-image.css-2i84d9[src='/images/quality-hat-model.jpg']`);
+    } 
 
 }
 module.exports = new HomePage();
